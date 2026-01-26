1f83e9638031310f249fd490c68cd9f6cbe20a8c
Zach-Sten <zach.stensland@ucsf.edu>
2026-01-26 13:37:31 -0800

    updating setup for alpha release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-anndata-visualizer",
-    version="0.1.0",
+    version="0.1.0-beta",
     author="Zachary Stensland",
     author_email="zach.stensland@ucsf.edu",
     description="Interactive spatial plotting for AnnData single-cell data",
